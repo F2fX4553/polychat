09174f1a9b4c7f35f27faf716fc106b492b053fa
F2fXOfficial <50016795+F2fX4553@users.noreply.github.com>
2025-03-17 07:41:21 +0100

    Delete wsgi.py

--- wsgi.py
+++
@@ -1,9 +0,0 @@
-import sys
-
-# add your project directory to the sys.path
-project_home = '/home/polychat'
-if project_home not in sys.path:
-    sys.path = [project_home] + sys.path
-
-# import flask app but need to call it "application" for WSGI to work
-from server import app as application  # noqa
